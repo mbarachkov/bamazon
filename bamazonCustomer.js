09e884ba98fb885bcbce6b8acb934416b650b1d4
Michael Barachkov <mike.barachkov@outlook.com>
2019-06-26 20:24:27 -0700

    got the response from ID coming back correct

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const inquirer = require("inquirer")
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -10,13 +11,64 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
-    afterConnection();
+    runSearch();
 });
-function afterConnection() {
-    connection.query("SELECT * FROM products", (err, data) => {
-        if (err) throw err;
-        console.log(data[0])
-        connection.end();
-    })
+
+
+function runSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Find items by ID",
+                "exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "Find items by ID":
+                    productSearch();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
 }
 
+// function productSearch() {
+//     inquirer
+//         .prompt({
+//             name: "PRIMARY KEY",
+//             type: "input",
+//             message: "What is the ID of the product you are looking for?"
+//         })
+//         .then(function (answer) {
+//             var query = "SELECT * FROM products WHERE ?";
+//             connection.query(query, { products: answer.product_name }, function (err, res) {
+//                 if (err) throw err;
+//                 for (var i = 0; i < res.length; i++) {
+//                     console.log(res[i].product_name);
+//                 }
+//                 runSearch();
+//             });
+//         });
+// }
+
+function productSearch() {
+    inquirer
+      .prompt({
+        type: "input",
+        message: "What is the ID of the item you are looking for?",
+        name: "id"
+      }).then(answer => {
+        connection.query("SELECT * FROM products WHERE ?", answer, (err, data)=> {
+          if(err) throw err;
+          console.table(data);
+          runSearch();
+        });
+      });
+  }
